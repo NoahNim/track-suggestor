f76c48db00e849e7f53b07435b4264940a3fa3aa
Noah Medoff <noahmedoff@Noahs-MacBook-Pro.local>
2018-03-23 09:08:15 -0700

    this starts the function to grab the selected radio button. currently this just console logs it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
+  $("form#track-survey").submit(function(event){
+    var userInterest = $("div#user-interest input:checked").val();
+    console.log(userInterest);
 
+    
+  });
 });
