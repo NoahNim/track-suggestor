09fcea62542e87e1715a9b74dca0a6f610138ce9
Noah Medoff <noahmedoff@Noahs-MacBook-Pro.local>
2018-03-23 09:26:36 -0700

    This made it so the Cdiv, PHPdiv, and Jdiv are hidden by default in CSS. It then adds functionality in jQuery to button submit that uses an if elseif statement to check which radio button the user selected and shows the appropriate div for their response. The functionality for the website is complete

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@ $(document).ready(function() {
   $("form#track-survey").submit(function(event){
     var userInterest = $("div#user-interest input:checked").val();
     console.log(userInterest);
-
-    
+    event.preventDefault();
+    if (userInterest === "C") {
+      $("#CDiv").show();
+      $("#PHPDiv").hide();
+      $("#JDiv").hide();
+    } else if (userInterest === "PHP") {
+      $("#PHPDiv").show();
+      $("#CDiv").hide();
+      $("#JDiv").hide();
+    } else if (userInterest === "JS") {
+      $("#JDiv").show();
+      $("#PHPDiv").hide();
+      $("#CDiv").hide();
+    }
   });
 });
