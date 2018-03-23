fae8e167c650f70186ac1e8eec75c89405e07e80
Noah Medoff <noahmedoff@Noahs-MacBook-Pro.local>
2018-03-23 10:19:32 -0700

    Added another div that displays the name the user entered

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
   $("form#track-survey").submit(function(event){
     event.preventDefault();
     var userInterest = $("div#user-interest input:checked").val();
+    var userName = $("#user-name").val();
 
+    console.log(userName);
+    $("#survey-finished").show();
+    $("#great-job").text("Awesome! " + userName);
+
+    // This is the if else statement that shows a div based on the users selection.
     if (userInterest === "C") {
       $("#CDiv").show();
       $("#PHPDiv").hide();
