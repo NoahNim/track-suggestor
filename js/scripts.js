2fd1f93d5970fa172b7dbf584627c65a1020b40e
Noah Medoff <noahmedoff@Noahs-MacBook-Pro.local>
2018-03-23 11:20:49 -0700

    added in styling to website, created borders for elements and centered them

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   $("form#track-survey").submit(function(event){
     event.preventDefault();
+
+    // These store user input on the webpage into JavaScript for use in this function
     var userInterest = $("div#user-interest input:checked").val();
     var userName = $("#user-name").val();
 
-    console.log(userName);
+    // This will make the survey finished div appear
     $("#survey-finished").show();
-    $("#great-job").text("Awesome! " + userName);
+    $("#great-job").text("Awesome! " + userName + ",");
 
     // This is the if else statement that shows a div based on the users selection.
     if (userInterest === "C") {
